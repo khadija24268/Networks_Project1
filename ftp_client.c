f03e2b8a5a79d161bf6bd716f9539e65e426b29f
khadijakhalid24 <125192824+khadijakhalid24@users.noreply.github.com>
2024-05-31 14:55:16 +0400

    USER and PASS working

--- ftp_client.c
+++ ftp_client.c
@@ -1,51 +1,188 @@
-#include <netdb.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
 
+void trim_newline(char *str) {
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
+
+void send_command_and_wait(int sock, const char *command) {
+    char full_command[MAX + 2];
+    snprintf(full_command, sizeof(full_command), "%s\r\n", command);
+    send(sock, full_command, strlen(full_command), 0);
+
+    char response[MAX];
+    bzero(response, MAX);
+    recv(sock, response, MAX, 0);
+    printf("%s", response);
+}
+
+int setup_data_connection(int *data_socket, int *client_port) {
+    *data_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (*data_socket == -1) {
+        printf("Data socket creation failed...\n");
+        return -1;
+    }
+
+    struct sockaddr_in data_addr;
+    data_addr.sin_family = AF_INET;
+    data_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    for (*client_port = 8081; *client_port < 8100; (*client_port)++) {
+        data_addr.sin_port = htons(*client_port);
+        if (bind(*data_socket, (SA*)&data_addr, sizeof(data_addr)) == 0) {
+            if (listen(*data_socket, 1) == 0) {
+                return 0;
+            }
+        }
+    }
+
+    close(*data_socket);
+    return -1;
+}
+
+
+int handle_data_connection(int data_socket, const char *filename, int is_retr) {
+    struct sockaddr_in cli;
+    int len = sizeof(cli);
+    int conn = accept(data_socket, (SA*)&cli, &len);
+    if (conn < 0) {
+        printf("Failed to accept data connection\n");
+        return -1;
+    }
+
+    FILE *file = NULL;
+    if (is_retr) {
+        file = fopen(filename, "wb");
+        if (!file) {
+            printf("Failed to open file for writing: %s\n", filename);
+            close(conn);
+            return -1;
+        }
+    } else {
+        file = fopen(filename, "rb");
+        if (!file) {
+            printf("Failed to open file for reading: %s\n", filename);
+            close(conn);
+            return -1;
+        }
+    }
+
+    char buffer[MAX];
+    int bytes;
+    if (is_retr) {
+        while ((bytes = recv(conn, buffer, sizeof(buffer), 0)) > 0) {
+            fwrite(buffer, 1, bytes, file);
+        }
+    } else {
+        while ((bytes = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+            send(conn, buffer, bytes, 0);
+        }
+    }
+
+    fclose(file);
+    close(conn);
+    close(data_socket);  // Ensure the data socket is closed here
+
+    // Wait for the server's final response
+    char response[MAX];
+    bzero(response, MAX);
+    recv(data_socket, response, MAX, 0);
+    printf("%s", response);
+
+    return 0;
+}
+
+
 void func(int sock) {
     char buff[MAX];
-    int n;
+    int data_socket = -1, client_port = -1;
+
     for (;;) {
-        bzero(buff, sizeof(buff));
         printf("ftp> ");
-        n = 0;
-        while ((buff[n++] = getchar()) != '\n');
-        send(sock, buff, sizeof(buff), 0);
-        bzero(buff, sizeof(buff));
-        recv(sock, buff, sizeof(buff), 0);
-        printf("%s", buff);
-        if ((strncmp(buff, "221", 3)) == 0) {
-            printf("Client Exit...\n");
-            break;
+        bzero(buff, MAX);
+        fgets(buff, MAX, stdin);
+        trim_newline(buff);
+
+        char* command = strtok(buff, " ");
+        char* argument = strtok(NULL, " ");
+
+        if (command != NULL) {
+            if (strcmp(command, "RETR") == 0 || strcmp(command, "STOR") == 0 || strcmp(command, "LIST") == 0) {
+                if (setup_data_connection(&data_socket, &client_port) != -1) {
+                    char port_command[MAX];
+                    sprintf(port_command, "PORT 127,0,0,1,%d,%d", client_port / 256, client_port % 256);
+                    send_command_and_wait(sock, port_command);
+                } else {
+                    printf("Failed to setup data connection\n");
+                    continue;
+                }
+            }
+
+            char command_with_args[MAX + 5];
+            if (argument) {
+                snprintf(command_with_args, sizeof(command_with_args), "%s %s", command, argument);
+            } else {
+                strncpy(command_with_args, command, sizeof(command_with_args));
+            }
+            send_command_and_wait(sock, command_with_args);
+
+            if (strncmp(buff, "QUIT", 4) == 0) {
+                printf("Client Exit...\n");
+                break;
+            }
+
+            if (data_socket != -1) {
+                if (strcmp(command, "RETR") == 0) {
+                    handle_data_connection(data_socket, argument, 1);
+                } else if (strcmp(command, "STOR") == 0) {
+                    handle_data_connection(data_socket, argument, 0);
+                } else if (strcmp(command, "LIST") == 0) {
+                    handle_data_connection(data_socket, "listing.txt", 1);
+                }
+                data_socket = -1;
+            }
         }
     }
 }
 
+
 int main() {
     int sock;
-    struct sockaddr_in servaddr, cli;
+    struct sockaddr_in servaddr;
+
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
-        printf("socket creation failed...\n");
+        printf("Socket creation failed...\n");
         exit(0);
     } else {
         printf("Socket successfully created..\n");
     }
+
+    memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servaddr.sin_port = htons(PORT);
+
     if (connect(sock, (SA*)&servaddr, sizeof(servaddr)) != 0) {
-        printf("connection with the server failed...\n");
+        printf("Connection with the server failed...\n");
         exit(0);
     } else {
-        printf("connected to the server..\n");
+        printf("Connected to the server..\n");
     }
+
     func(sock);
     close(sock);
+    return 0;
 }
--- ftp_server.c
+++ ftp_server.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
 #include <dirent.h>
+#include <errno.h>
+#include <ctype.h>
+
 #define MAX 80
 #define PORT 8080
+#define MAX_CLIENTS 30
 #define SA struct sockaddr
 
 typedef struct {
@@ -22,22 +27,52 @@ int user_count = 0;
 void load_users() {
     FILE *file = fopen("users.txt", "r");
     if (!file) {
-        printf("Could not open users.txt\n");
+        perror("Could not open users.txt");
         exit(1);
     }
 
-    while (fscanf(file, "%s %s", users[user_count].username, users[user_count].password) != EOF) {
+    while (fscanf(file, "%s %s", users[user_count].username, users[user_count].password) != EOF && user_count < 100) {
+        printf("Loaded user: %s\n", users[user_count].username);
         user_count++;
     }
 
     fclose(file);
 }
 
-int authenticate(char *username, char *password) {
+void trim_newline(char *str) {
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
+
+void trim_whitespace(char *str) {
+    char *end;
+    while (isspace((unsigned char)*str)) str++;
+    if (*str == 0) return;
+
+    end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end)) end--;
+    end[1] = '\0';
+}
+
+int check_username(char *username) {
+    trim_whitespace(username);
+    trim_newline(username);
+
     for (int i = 0; i < user_count; i++) {
-        if (strcmp(username, users[i].username) == 0 && strcmp(password, users[i].password) == 0) {
-            return 1;
+        if (strcmp(username, users[i].username) == 0) {
+            return i;
         }
+    }
+    return -1;
+}
+
+int authenticate(int user_index, char *password) {
+    trim_whitespace(password);
+    trim_newline(password);
+    if (strcmp(users[user_index].password, password) == 0) {
+        return 1;
     }
     return 0;
 }
@@ -46,167 +81,317 @@ void send_response(int client_socket, const char *response) {
     send(client_socket, response, strlen(response), 0);
 }
 
-void handle_data_connection(int client_socket, char *command, char *argument);
-void func(int client_socket) {
-    char buff[MAX];
-    int n;
-    int logged_in = 0;
-    char username[MAX];
+int start_data_connection(char *client_ip, int client_port) {
+    int data_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (data_socket == -1) {
+        perror("Failed to create data socket");
+        return -1;
+    }
 
-    for (;;) {
+    struct sockaddr_in data_addr;
+    memset(&data_addr, 0, sizeof(data_addr));
+    data_addr.sin_family = AF_INET;
+    data_addr.sin_addr.s_addr = inet_addr(client_ip);
+    data_addr.sin_port = htons(client_port);
+
+    for (int attempt = 0; attempt < 5; attempt++) {
+        if (connect(data_socket, (SA*)&data_addr, sizeof(data_addr)) == 0) {
+            return data_socket;
+        }
+        perror("Failed to connect data socket, retrying");
+        sleep(1);
+    }
+
+    close(data_socket);
+    return -1;
+}
+
+void handle_retr(int client_socket, int data_socket, char *filename) {
+    trim_whitespace(filename);
+    trim_newline(filename);
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        send_response(client_socket, "550 File not found.\n");
+        return;
+    }
+
+    send_response(client_socket, "150 Opening binary mode data connection for file transfer.\n");
+
+    char buffer[1024];
+    int bytes_read;
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        send(data_socket, buffer, bytes_read, 0);
+    }
+    fclose(file);
+    close(data_socket);
+    send_response(client_socket, "226 Transfer complete.\n");
+}
+
+void handle_stor(int client_socket, int data_socket, char *filename) {
+    trim_whitespace(filename);
+    trim_newline(filename);
+    FILE *file = fopen(filename, "wb");
+    if (!file) {
+        send_response(client_socket, "550 Could not open file.\n");
+        return;
+    }
+
+    send_response(client_socket, "150 Opening binary mode data connection for file reception.\n");
+
+    char buffer[1024];
+    int bytes_received;
+    while ((bytes_received = recv(data_socket, buffer, sizeof(buffer), 0)) > 0) {
+        fwrite(buffer, 1, bytes_received, file);
+    }
+    fclose(file);
+    close(data_socket);
+    send_response(client_socket, "226 Transfer complete.\n");
+}
+
+void handle_cwd(int client_socket, char *directory) {
+    trim_whitespace(directory);
+    trim_newline(directory);
+    if (chdir(directory) == 0) {
+        send_response(client_socket, "250 Directory successfully changed.\r\n");
+    } else {
+        send_response(client_socket, "550 Failed to change directory.\r\n");
+    }
+}
+
+void handle_pwd(int client_socket) {
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        char msg[1060];
+        sprintf(msg, "257 \"%s\" is the current directory.\r\n", cwd);
+        send_response(client_socket, msg);
+    } else {
+        send_response(client_socket, "550 Get current directory failed.\r\n");
+    }
+}
+
+void handle_list(int client_socket, int data_socket) {
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(".");
+    if (d) {
+        send_response(client_socket, "150 File status okay; about to open data connection.\r\n");
+        while ((dir = readdir(d)) != NULL) {
+            send(data_socket, dir->d_name, strlen(dir->d_name), 0);
+            send(data_socket, "\r\n", 2, 0);
+        }
+        closedir(d);
+        close(data_socket);
+        send_response(client_socket, "226 Transfer completed.\r\n");
+    } else {
+        send_response(client_socket, "550 Failed to open directory.\r\n");
+    }
+}
+
+void handle_client(int client_socket) {
+    char buff[MAX];
+    int logged_in = 0;
+    int user_index = -1;
+    int data_socket = -1;
+    char client_ip[16];
+    int client_port = -1;
+
+    while (1) {
         bzero(buff, MAX);
-        recv(client_socket, buff, sizeof(buff), 0);
+        int recv_length = recv(client_socket, buff, sizeof(buff), 0);
+        if (recv_length <= 0) {
+            printf("Failed to read data from client or client disconnected\n");
+            close(client_socket);
+            return;
+        }
+
         printf("From client: %s\n", buff);
 
         char *command = strtok(buff, " ");
         char *argument = strtok(NULL, " ");
 
-        if (strncmp(command, "USER", 4) == 0) {
-            strcpy(username, argument);
-            send_response(client_socket, "331 Username OK, need password.\n");
-        } else if (strncmp(command, "PASS", 4) == 0) {
-            if (authenticate(username, argument)) {
-                logged_in = 1;
-                send_response(client_socket, "230 User logged in, proceed.\n");
+        if (command == NULL) {
+            send_response(client_socket, "500 Syntax error, command unrecognized.\n");
+            continue;
+        }
+
+        trim_newline(command);
+        if (argument) {
+            trim_newline(argument);
+        }
+
+        if (strcmp(command, "QUIT") == 0) {
+            send_response(client_socket, "221 Service closing control connection.\n");
+            close(client_socket);
+            return;
+        }
+
+        if (!logged_in) {
+            if (strcmp(command, "USER") == 0) {
+                if (argument) {
+                    user_index = check_username(argument);
+                    if (user_index != -1) {
+                        send_response(client_socket, "331 Username OK, need password.\n");
+                    } else {
+                        send_response(client_socket, "530 User not found.\n");
+                    }
+                } else {
+                    send_response(client_socket, "501 Syntax error in parameters or arguments.\n");
+                }
+            } else if (strcmp(command, "PASS") == 0) {
+                if (user_index != -1 && argument) {
+                    if (authenticate(user_index, argument)) {
+                        logged_in = 1;
+                        send_response(client_socket, "230 User logged in, proceed.\n");
+                    } else {
+                        send_response(client_socket, "530 Wrong password.\n");
+                    }
+                } else {
+                    send_response(client_socket, "503 Bad sequence of commands.\n");
+                }
             } else {
-                send_response(client_socket, "530 Not logged in.\n");
+                send_response(client_socket, "530 Please login with USER and PASS.\n");
             }
-        } else if (logged_in) {
-            if (strncmp(command, "PORT", 4) == 0) {
-                send_response(client_socket, "200 PORT command successful.\n");
-            } else if (strncmp(command, "RETR", 4) == 0 || strncmp(command, "STOR", 4) == 0 || strncmp(command, "LIST", 4) == 0) {
-                handle_data_connection(client_socket, command, argument);
-            } else if (strncmp(command, "QUIT", 4) == 0) {
-                send_response(client_socket, "221 Service closing control connection.\n");
-                break;
+        } else {
+            if (strcmp(command, "PORT") == 0) {
+                if (argument) {
+                    int h1, h2, h3, h4, p1, p2;
+                    sscanf(argument, "%d,%d,%d,%d,%d,%d", &h1, &h2, &h3, &h4, &p1, &p2);
+                    sprintf(client_ip, "%d.%d.%d.%d", h1, h2, h3, h4);
+                    client_port = p1 * 256 + p2;
+                    printf("Client IP: %s, Client Port: %d\n", client_ip, client_port);
+                    data_socket = start_data_connection(client_ip, client_port);
+                    if (data_socket != -1) {
+                        send_response(client_socket, "200 PORT command successful.\n");
+                    } else {
+                        send_response(client_socket, "425 Can't open data connection.\n");
+                    }
+                } else {
+                    send_response(client_socket, "501 Syntax error in parameters or arguments.\n");
+                }
+            } else if (strcmp(command, "RETR") == 0) {
+                if (data_socket != -1 && argument) {
+                    handle_retr(client_socket, data_socket, argument);
+                    data_socket = -1;
+                } else {
+                    send_response(client_socket, "425 Use PORT command to specify data connection first.\n");
+                }
+            } else if (strcmp(command, "STOR") == 0) {
+                if (data_socket != -1 && argument) {
+                    handle_stor(client_socket, data_socket, argument);
+                    data_socket = -1;
+                } else {
+                    send_response(client_socket, "425 Use PORT command to specify data connection first.\n");
+                }
+            } else if (strcmp(command, "LIST") == 0) {
+                if (data_socket != -1) {
+                    handle_list(client_socket, data_socket);
+                    data_socket = -1;
+                } else {
+                    send_response(client_socket, "425 Use PORT command to specify data connection first.\n");
+                }
+            } else if (strcmp(command, "CWD") == 0) {
+                if (argument) {
+                    if (chdir(argument) == 0) {
+                        send_response(client_socket, "250 Directory successfully changed.\n");
+                    } else {
+                        send_response(client_socket, "550 Failed to change directory.\n");
+                    }
+                } else {
+                    send_response(client_socket, "501 Syntax error in parameters or arguments.\n");
+                }
+            } else if (strcmp(command, "PWD") == 0) {
+                char cwd[1024];
+                if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                    char msg[1060];
+                    sprintf(msg, "257 \"%s\" is the current directory.\n", cwd);
+                    send_response(client_socket, msg);
+                } else {
+                    send_response(client_socket, "550 Get current directory failed.\n");
+                }
             } else {
-                send_response(client_socket, "202 Command not implemented.\n");
+                send_response(client_socket, "502 Command not implemented.\n");
             }
-        } else {
-            send_response(client_socket, "530 Not logged in.\n");
         }
     }
-}
-
-void handle_data_connection(int client_socket, char *command, char *argument) {
-    int data_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (data_socket == -1) {
-        printf("data socket creation failed...\n");
-        send_response(client_socket, "425 Can't open data connection.\n");
-        return;
-    }
-
-    struct sockaddr_in data_addr;
-    data_addr.sin_family = AF_INET;
-    data_addr.sin_addr.s_addr = INADDR_ANY;
-    data_addr.sin_port = htons(PORT + 1);
-
-    if (bind(data_socket, (SA*)&data_addr, sizeof(data_addr)) != 0) {
-        printf("data socket bind failed...\n");
-        send_response(client_socket, "425 Can't open data connection.\n");
-        close(data_socket);
-        return;
-    }
-
-    if (listen(data_socket, 1) != 0) {
-        printf("data socket listen failed...\n");
-        send_response(client_socket, "425 Can't open data connection.\n");
-        close(data_socket);
-        return;
-    }
-
-    send_response(client_socket, "150 File status okay; about to open data connection.\n");
-
-    struct sockaddr_in client;
-    int len = sizeof(client);
-    int data_client_socket = accept(data_socket, (SA*)&client, &len);
-    if (data_client_socket < 0) {
-        printf("data socket accept failed...\n");
-        send_response(client_socket, "425 Can't open data connection.\n");
-        close(data_socket);
-        return;
-    }
-
-    if (strncmp(command, "RETR", 4) == 0) {
-        FILE *file = fopen(argument, "rb");
-        if (file == NULL) {
-            send_response(client_socket, "550 No such file or directory.\n");
-        } else {
-            char file_buffer[1024];
-            size_t bytes_read;
-            while ((bytes_read = fread(file_buffer, 1, sizeof(file_buffer), file)) > 0) {
-                send(data_client_socket, file_buffer, bytes_read, 0);
-            }
-            fclose(file);
-            send_response(client_socket, "226 Transfer completed.\n");
-        }
-    } else if (strncmp(command, "STOR", 4) == 0) {
-        FILE *file = fopen(argument, "wb");
-        if (file == NULL) {
-            send_response(client_socket, "550 No such file or directory.\n");
-        } else {
-            char file_buffer[1024];
-            size_t bytes_received;
-            while ((bytes_received = recv(data_client_socket, file_buffer, sizeof(file_buffer), 0)) > 0) {
-                fwrite(file_buffer, 1, bytes_received, file);
-            }
-            fclose(file);
-            send_response(client_socket, "226 Transfer completed.\n");
-        }
-    } else if (strncmp(command, "LIST", 4) == 0) {
-        DIR *dir = opendir(".");
-        if (dir == NULL) {
-            send_response(client_socket, "550 No such file or directory.\n");
-        } else {
-            struct dirent *entry;
-            while ((entry = readdir(dir)) != NULL) {
-                send(data_client_socket, entry->d_name, strlen(entry->d_name), 0);
-                send(data_client_socket, "\n", 1, 0);
-            }
-            closedir(dir);
-            send_response(client_socket, "226 Transfer completed.\n");
-        }
-    }
-
-    close(data_client_socket);
-    close(data_socket);
 }
 
 int main() {
     load_users();
 
-    int server_socket, client_socket, len;
+    int server_socket, client_socket[MAX_CLIENTS], max_sd, sd, activity, i;
     struct sockaddr_in servaddr, cli;
+    fd_set readfds;
+
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
-        printf("socket creation failed...\n");
-        exit(0);
-    } else {
-        printf("Socket successfully created..\n");
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
     }
+    printf("Socket successfully created..\n");
+
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
+
     if ((bind(server_socket, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-        printf("socket bind failed...\n");
-        exit(0);
-    } else {
-        printf("Socket successfully binded..\n");
+        perror("socket bind failed");
+        exit(EXIT_FAILURE);
     }
-    if ((listen(server_socket, 5)) != 0) {
-        printf("Listen failed...\n");
-        exit(0);
-    } else {
-        printf("Server listening..\n");
+    printf("Socket successfully binded..\n");
+
+    listen(server_socket, 5);
+    printf("Server listening..\n");
+
+    for (i = 0; i < MAX_CLIENTS; i++) {
+        client_socket[i] = 0;
     }
-    len = sizeof(cli);
-    client_socket = accept(server_socket, (SA*)&cli, &len);
-    if (client_socket < 0) {
-        printf("server accept failed...\n");
-        exit(0);
-    } else {
-        printf("server accept the client...\n");
+
+    while (1) {
+        FD_ZERO(&readfds);
+        FD_SET(server_socket, &readfds);
+        max_sd = server_socket;
+
+        for (i = 0; i < MAX_CLIENTS; i++) {
+            sd = client_socket[i];
+            if (sd > 0) {
+                FD_SET(sd, &readfds);
+            }
+            if (sd > max_sd) {
+                max_sd = sd;
+            }
+        }
+
+        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+
+        if ((activity < 0) && (errno != EINTR)) {
+            printf("select error");
+        }
+
+        if (FD_ISSET(server_socket, &readfds)) {
+            int len = sizeof(cli);
+            int new_socket = accept(server_socket, (SA*)&cli, &len);
+            if (new_socket < 0) {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+            printf("New connection, socket fd is %d, ip is : %s, port : %d \n", new_socket, inet_ntoa(cli.sin_addr), ntohs(cli.sin_port));
+
+            for (i = 0; i < MAX_CLIENTS; i++) {
+                if (client_socket[i] == 0) {
+                    client_socket[i] = new_socket;
+                    printf("Adding to list of sockets as %d\n", i);
+                    break;
+                }
+            }
+        }
+
+        for (i = 0; i < MAX_CLIENTS; i++) {
+            sd = client_socket[i];
+
+            if (FD_ISSET(sd, &readfds)) {
+                handle_client(sd);
+            }
+        }
     }
-    func(client_socket);
-    close(server_socket);
+    return 0;
 }
